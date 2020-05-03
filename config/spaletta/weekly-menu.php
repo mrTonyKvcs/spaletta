f8e02050d1122cd8455e89047800f9e8c8fc760b
tony <attila.kovacs92@gmail.com>
2020-05-03 19:29:43 +0200

    weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. április 28. - május 02.',
+    'sub_title' => '2020. május 05. - 09.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Sütőtök krémleves (G,L)',
-                'Csülkös bableves',
-                'Grízgaluska leves (L)'
+                'Frankfurti leves tejfölösen virslivel',
+                'Marhapofa gulyásleves',
+                'Brokkoli krémleves, fokhagymás krutonnal'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bácskai rizseshús (kovászos uborka) (L)',
-                'Lasagne',
-                'Tépett húsos szendvics – hasáb',
-                'Császármorzsa',
+                'Tex-mex csirke - csónak burgonya, ketchup',
+                'Osso bucco , sáfrányos rizottóval',
+                'Sertés tokány Budapest módra parázs burgonyával',
+                'Túrós derelye - fahéjas tejföl',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Zöldborsó leves galuskával (L)',
-            'Rántott szelet – majonézes burgonyasaláta',
-            'Csokis brownie málnaöntet'
+            'Vadas leves, marhahús kockával és kenyérpudinggal',
+            'Sült kacsacomb - hagymás tört burgonya, lilakáposzta püré',
+            'Meggyes piskóta'
         ]
     ]
 ];

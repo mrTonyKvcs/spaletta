1369f35f701b84970afb8c99b932ce946656f13a
tony <attila.kovacs92@gmail.com>
2020-05-11 13:04:04 +0200

    weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. május 05. - 09.',
+    'sub_title' => '2020. május 12. - 16.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Frankfurti leves tejfölösen virslivel',
-                'Marhapofa gulyásleves',
-                'Brokkoli krémleves, fokhagymás krutonnal'
+                'Tárkonyos sertésragu leves (G)',
+                'Vegyes gyümölcsleves (L;G)',
+                'Csirkehúsleves (L) (tészta nélkül gluténmentes)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Tex-mex csirke - csónak burgonya, ketchup',
-                'Osso bucco , sáfrányos rizottóval',
-                'Sertés tokány Budapest módra parázs burgonyával',
-                'Túrós derelye - fahéjas tejföl',
+                'Svéd húsgolyó – burgonyapürével áfonya mártással (L)',
+                'Aranygaluska vaníliasodó',
+                'Székelykáposzta (G ; L)',
+                'Pulykás zöldséges  sült tészta (L)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Vadas leves, marhahús kockával és kenyérpudinggal',
-            'Sült kacsacomb - hagymás tört burgonya, lilakáposzta püré',
-            'Meggyes piskóta'
+            'Egres krémleves fahéjas habgaluska',
+            'Borjúpaprikás tojásos nokedli - fejes saláta',
+            'Gundel palacsinta'
         ]
     ]
 ];

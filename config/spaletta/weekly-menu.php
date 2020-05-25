0df60f6bad1ad6c990f970bb7d3d4d2e9ee3213e
tony <attila.kovacs92@gmail.com>
2020-05-25 09:21:54 +0200

    change menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. május 19. - 23.',
+    'sub_title' => '2020. május 26. - 30.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Babgulyás (L)',
-                'Savanyú tojásleves',
-                'Hideg meggyleves (G;L)'
+                'Csorba leves (G;L)',
+                'Görög gyümölcsleves (G;L)',
+                'Csontleves csigatésztával (L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Pankó morzsás sertés csíkok- tavaszi zöldbabfőzelék',
-                'Harcsapaprikás – túrós csusza',
-                'Mini Margherita pizza (20cm)',
-                'Baconbe tekert sertés szűz – burgonya gratin (G)',
+                'Tejszínes gombás csirkemell - rizs (G;L)',
+                'Zöldséggel töltött rántott palacsinta kapros joghurtos öntet saláta',
+                'Máglyarakás',
+                'Vörösboros marha pörkölt – galuska Uborka saláta (L)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Mentás zöldborsó krémleves (G;L)',
-            'Csirkecomb vadasan – knédli',
-            'Rizsfelfújt erdei gyümölcsragu'
+            'Spárga krémleves bacon chips (G;L)',
+            'Rántott harcsa pagodakarfiol püré zöldborsós bulgur',
+            'Gyümölcsös pavlova (G;L)'
         ]
     ]
 ];

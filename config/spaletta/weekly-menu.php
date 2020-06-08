724c6eef825f6f6d783d9ab06eb999cd73bc46f1
tony <attila.kovacs92@gmail.com>
2020-06-08 18:17:13 +0200

    change weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. június 02. - 06.',
+    'sub_title' => '2020. június 09. - 13.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Eper krémleves rebarbarával (G;L)',
-                'Zöldborsó leves csibehússal (L)',
-                'Korhelyleves csülökkel (G;L)'
+                'Burgonyakrémleves kolbászmorzsa köménymagos tejfölhabbal (G)',
+                'Becsinált leves (L)',
+                'Hideg almaleves (G L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bolognai tészta',
-                'Pulled pork szendvics – hasáb burgonya',
-                'Rántott camembert – burgonyapüré - áfonyalekvár',
-                'Paradicsomos mozzarellás csirkemell – rukkolás salátával',
+                'Sous vide-olt tarja steak - brokkolis fóliás burgonya füstölt sajt (G)',
+                'Hideg salátafőzelék – Buggyantott tojás (G L)',
+                'Vargabéles rétes vanília öntet tejszínhabbal',
+                'Hortobágyi palacsinta',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Zöldségleves reszelt tésztával (L) (tészta nélkül G mentes)',
-            'Vasalt csirkecomb filé – jázmin rizs majonézes kukoricasaláta (G)',
-            'Tiramisu'
+            'Nyírségi gombócleves (L)',
+            'Snidlinges túrós sertéskaraj pecsenyelével – petrezselymes burgonya (G)',
+            'Profiterol csokiöntet'
         ]
     ]
 ];

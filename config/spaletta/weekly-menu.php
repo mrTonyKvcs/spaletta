6a9dbd87e21652be296dafdc5a71adea2b31dacc
tony <attila.kovacs92@gmail.com>
2020-06-22 11:56:27 +0200

    change weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. június 16. - 20.',
+    'sub_title' => '2020. június 23. - 27.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Zöldségleves húsgolyóval (L)',
-                'Hideg meggyleves csoki habbal (G)',
-                'Tarhonyaleves borjúhússal (L)'
+                'Brokkolis karfiolkrém- levesgyöngy (L)',
+                'Palócleves (L)',
+                'Csontleves- zöldségek, csigatészta (L,G)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Citrusos fűszeres csirketokány – tészta (L)',
-                'BBQ oldalas Steak burgonya coleslaw saláta (G)',
-                'Parmezános pulykamedalion rántva – rizibizi',
-                'Oliva olajos saláta rántott mozzarella',
+                'Cézár saláta – csirkemell (G)',
+                'Zöldséges udon tészta (L)',
+                'Rizsfelfújt erdei gyümölcsragúval',
+                'Magyaros töltött borda – hagymás burgonya, kovászos uborka (G)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Gazpacho (L)',
-            'Halnyárs – csónakburgonya tzatziki (G)',
-            'Málnás kosár túrófagyi'
+            'Vegyes gyümölcsleves (G,L)',
+            'Gyros tál- hasábburgonya, saláta (G,L)',
+            'Csokoládé mousse (G,L)'
         ]
     ]
 ];

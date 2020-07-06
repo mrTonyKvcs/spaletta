ca683d87c63697eb4c3514373e0fe445708c8d17
tony <attila.kovacs92@gmail.com>
2020-07-06 10:28:39 +0200

    weekly

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. június 30. - július 04.',
+    'sub_title' => '2020. július 07. - 11.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Gulyásleves (L)',
-                'Hideg citromleves (G;L)',
-                'Karfiol leves'
+                'Frankfurti leves (G;L)',
+                'Paradicsomleves (L)',
+                'Sárgadinnye krémleves (G)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Rántott csirkecomb meggymártás (L)',
-                'Pestos mozzarellás tészta mandulával',
-                'Magyaros sertésborda pirított polentával (G;L)',
-                'Mákos guba vaníliaöntet',
+                'Tarhonyás hús csemege uborka (L)',
+                'Meggyes vaníliás palacsinta',
+                'Toledói csirke ragu hasáb (G)',
+                'Padlizsán torony kuszkusz',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Kis húsos fazék (L)',
-            'Harcsa steak paprikás mártással - pirított kenyér puding(L)',
-            'Vanília fagyi tökmaggal tökmagolajjal (G)'
+            'Savanyú tojásleves buggyantott fürjtojással (L)',
+            'Rántott szelet petrezselymes burgonya almakompót (L)',
+            'Madártej (G)'
         ]
     ]
 ];

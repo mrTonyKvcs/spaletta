8a0c0d6e32759fd9b76a414fac6ffa0080147ca3
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-08-23 23:48:53 +0200

    new weekly menu

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,28 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. augusztus 18. - 2020. augusztus 19.',
+    'sub_title' => '2020. augusztus 25. - 2020. augusztus 29.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Erdei gyümölcskrém leves pannacotta (G)',
+                'Tejszínes padlizsánkrémleves- tortellini (L)',
+                'Újházi tyúkhúsleves (L)',
+                'Tarhonyaleves (L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Supreme csirke – répapüré édesköményes diós almás saláta (G)',
-                'Rakott burgonya (G, L)',
+                'Töltött paprika - paradicsommártás (L)',
+                'Paprikás csirke – galuska (uborkasaláta)',
+                'Magvas sajtgolyók – Quinoás szőlős saláta (G)',
+                'Almás rétes vanília öntet tejszínhab',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            '',
-            '',
-            ''
+            'Görög gyümölcsleves (G, L)',
+            'Rántott szelet – burgonyasaláta',
+            'Tiramisu'
         ]
     ]
 ];

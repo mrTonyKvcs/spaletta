d502388c88bb6aa3b8f0ddbf903811cf3d6458ae
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-09-14 11:07:37 +0200

    new weekly menu szept. 15-19.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. szeptember 8. - 2020. szeptember 12.',
+    'sub_title' => '2020. szeptember 15. - 2020. szeptember 19.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Zöldborsóleves (G,L)',
-                'Májgombócleves (L)',
-                'Vörösboros-szilvaleves (G,L)',
+                'Tárkonyos pulyka raguleves (G,L)',
+                'Almaleves (G,L)',
+                'Francia zöldségkrémleves, kruton (L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Rántott csirkemell,francia saláta',
-                'Bakonyi borda, galuska (L)',
-                'Túrógombóc, fahéjas tejföl',
-                'Csőben sült zöldségek, sajtmártás (G)',
+                'Paradicsomos-mozzarellás csirke, rizs',
+                'Zöldségestócsni, saláta (L)',
+                'Sertés aprópecsenye, jóasszony módra, pirított burgonya (L)',
+                'Bolognai penne (G)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Lebbencsleves (L)',
-            'Mustáros sertés flekken, steakburgonya (L)',
-            'Chia puding'
+            'Jókai bableves',
+            'Juhtúrós sztrapacska',
+            'Csokoládépuding'
         ]
     ]
 ];

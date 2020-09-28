1c7a2ade08579d11026b515cbec27e739553f75a
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-09-28 09:33:34 +0200

    new weekly menu 09.29.-10.03

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. szeptember 15. - 2020. szeptember 19.',
+    'sub_title' => '2020. szeptember 29. - 2020. október 3.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Magyaros gulyásleves (L)',
-                'Csontleves csigatésztával (L)',
-                'Pikáns vöröslencse krémleves (G, L)',
+                'Zöldborsóleves, húsgolyó (L)',
+                'Frankfurti leves (L)',
+                'Sütőtök-krémleves (G,L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bicskás pecsenye – hasábburgonya (G, L)',
-                'Rozmaringos mustáros csirkecomb – tepsis zöldségek (G, L)',
-                'Ázsiai zöldséges udon tészta (L)',
-                'Mákos guba- vanília sodó',
+                'Csirkés tortilla',
+                'Céklás rizottó rukkola, juhsajt',
+                'Sült oldalas. hagymás tört burgonya (G,L)',
+                'Magvas bundán sült harcsa, zöldséges gersli (L)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Kukorica krémleves (G, L)',
-            'Sertés cordon bleu– rizibizi',
-            'Gundel palacsinta'
+            'Húsosfazék, cérnametélt (L)',
+            'Pacalpörkölt, sós burgonya (G,L)',
+            'Keksztekercs'
         ]
     ]
 ];

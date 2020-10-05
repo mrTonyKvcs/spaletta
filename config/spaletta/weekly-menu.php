937e955350369957c2b42a67f41607e8c0100621
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-10-05 10:37:41 +0200

    new weekly menu 10.06-10.10

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. szeptember 29. - 2020. október 3.',
+    'sub_title' => '2020. október 06. - 2020. október 10.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Zöldborsóleves, húsgolyó (L)',
-                'Frankfurti leves (L)',
-                'Sütőtök-krémleves (G,L)',
+                'Legényfogó leves (G, L)',
+                'Erőleves palacsintával (L)',
+                'Körtés zeller krémleves (L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Csirkés tortilla',
-                'Céklás rizottó rukkola, juhsajt',
-                'Sült oldalas. hagymás tört burgonya (G,L)',
-                'Magvas bundán sült harcsa, zöldséges gersli (L)',
+                'Kijevi jércemell – Rizi-bizi',
+                'Rakott karfiol (G, L)',
+                'Cukkinis pesztós tészta',
+                'Korzikai apró pecsenye (G, L)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Húsosfazék, cérnametélt (L)',
-            'Pacalpörkölt, sós burgonya (G,L)',
-            'Keksztekercs'
+            'Bojtár leves (L)',
+            'Töltött csirke comb – tört burgonya (L)',
+            'Madártej (G)'
         ]
     ]
 ];

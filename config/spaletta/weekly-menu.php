375eb650e502b3ea15ddb3610f79d05a4e131a33
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-10-19 10:16:23 +0200

    new weekly menu 10.20-10.24.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. október 13. - 2020. október 17.',
+    'sub_title' => '2020. október 20. - 2020. október 24.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Palócleves (L)',
-                'Szőlőleves (G, L)',
-                'Karfiol leves (G, L)',
+                'Magyaros burgonyaleves (kolbászmorzsa)',
+                'Langyos szilvaleves (G, L)',
+                'Szárnyas aprólékleves (G, L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Stíriai metélt',
-                'Székelykáposzta (G, L)',
-                'Burgonyafőzelék - kolbász',
-                'Tejszínes gombás csirkemell – parmezános tarhonya',
+                'Rakott csülök (G)',
+                'Zöldséges currys csirke – rizs (G, L)',
+                'Sült pulykacomb – sütőtökös burgonyapüré (G)',
+                'Farsangi fánk baracklekvár',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Póréhagymás burgonya krémleves (L)',
-            'Lecsós tarja steak – rizs (G, L)',
-            'Kókuszkocka'
+            'Kapros tökleves (G, L)',
+            'Sertés cordon bleu – bulgur , vajon párolt  zöldség',
+            'Mákos mézes kocka'
         ]
     ]
 ];

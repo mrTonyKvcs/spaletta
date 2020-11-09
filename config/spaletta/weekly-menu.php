d0653254b53e2242fd9e67458ef36f74614e329d
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-11-09 14:15:02 +0100

    new weekly menu 11.10-11.14

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,32 +2,32 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. november 03. - 2020. november 07.',
+    'sub_title' => '2020. november 10. - 2020. november 14.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Nyírségi gombócleves (L)',
-                'Fahéjas almaleves (G, L)',
-                'Sárgaborsó krémleves csülökkel (G, L)',
+                'Francia hagymaleves (sajtos kruton)',
+                'Gulyásleves (G, L)',
+                'Zöldbableves (G, L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Bakonyi sertéstokány – házi galuska (L)',
-                'Karfiol steak – langyos kuszkusz saláta (L)',
-                'Szárnyas nyárs – hasábburgonya, tzatziki (G)',
-                'Rántott camembert – áfonyaíz, jázmin rizs',
+                'Fekete szezámos harcsa – petrezselymes burgonya, majonéz',
+                'Lencsés céklasaláta zöldséggel töltött palacsinta',
+                'Tarja steak pirított gomba – rizs (G, L)',
+                'Paprikás csirke – tészta (L)',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Grízgaluskaleves (L)',
-            'Lecsós sertés szűz – karikaburgonya (G, L)',
-            'Csokis brownie'
+            'Halászlé (gyufatészta) (L)',
+            'Sült túrós csusza',
+            'Tökös mákos rétes'
         ]
     ]
 ];

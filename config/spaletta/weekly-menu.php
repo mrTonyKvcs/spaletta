1d756239462126d9be14300e5c62087125c95eab
KovacsJ92 <jozsef.kovacs92@gmail.com>
2020-11-30 11:53:25 +0100

    new weekly 12.1-12.6

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,40 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2020. november 24. - 2020. november 29.',
+    'sub_title' => '2020. december 1. - 2020. december 6.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Tárkonyos sertés raguleves (G, L)',
-                'Tarhonyaleves (L)',
-                'Téli gyümölcsleves (G, L)',
+                'Kukorica krémleves (G, L)',
+                'Frankfurti leves (G, L)',
+                'Fürj tojásleves csibehússal (L)',
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Zöldséges sült tészta (L)',
-                'Joghurtos currys csirkemell – vajas ceruzabab, rizs',
-                'Csülök Pékné módra (G, L)',
-                'Vaníliás palacsinta eper ragu',
+                'Sajttal, sonkával töltött sertéskaraj – rizibizi, céklasaláta',
+                'Kókusztejes zöldborsófőzelék – májfelfújt (G, L)',
+                'Sült csirkecomb – törtburgonya forró meggyragu (G, L)',
+                'Mákos guba Erdei gyümölcsragu',
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Minestrone',
-            'Sertésborda vadasan – kenyérpuding',
-            'Almás pite'
+            'Újházi tyúkhúsleves (L)',
+            'Sült tarja – burgonyapüré, lyoni hagyma, birsalmakompót (G)',
+            'Gesztenyés kifli'
         ]
     ],
     'sunday-menu' => [
         'name'  => 'Vasárnapi menü',
         'items' => [
-            'Gulyásleves (G, L)',
-            'Párizsi csirkemell – Hasábburgonya, ketchup',
-            'Túrógombóc - gyümölcs ragu (ahogy mi készítjük)'
+            'Erőleves tojáskocsonyával (G, L)',
+            'Konfitált kacsacomb – Törtburgonya, párolt káposzta (G, L)',
+            'Somlói galuska'
         ]
     ]
 ];

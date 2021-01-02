a9d2840d4f8204dc3354550aaf838eadff3c05f3
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-01-02 10:41:38 +0100

    new weekly menu 01.05-01.10.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,34 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '',
+    'sub_title' => '2021.01.05 - 2021.01.10.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                '',
-                '',
+                'Citromos ragúleves (G)',
+                'Erőleves fridatto (G)',
+                'Paradicsomleves (L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                '',
-                '',
+                'Rakott karfiol (G, L)',
+                'Citrusos harcsa roston – Hasábburgonya, (G, L)',
+                'Máglyarakás',
+                'Rántott sajt – rizs, tartár'
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            '',
-            '',
-            ''
+            'Zöldségleves (G, L)',
+            'Hortobágyi húsos palacsinta',
+            'Kókuszkocka'
         ]
     ],
     'sunday-menu' => [
-        'name'  => '',
+        'name'  => 'Vasárnapi menü',
         'items' => [
+            'Tyúkhúsleves',
+            'Sertés bécsi – burgonyasaláta',
+            'Somlói galuska'
         ]
     ]
 ];
--- resources/views/partials/sections/_weekly-menu.blade.php
+++ resources/views/partials/sections/_weekly-menu.blade.php
@@ -26,41 +26,41 @@
                     </div>
                 </div>
             @endforeach
-            {{-- <div class="col-md-12" style="margin-bottom: 20px;"><hr></div> --}}
-            {{-- <div class="col-md-6" style=""> --}}
-                {{-- <div class="text-left title-base"> --}}
-                {{--     <hr> --}}
-                {{--     <div class="list-items" style="margin-bottom: 25px;"> --}}
-                {{--         @foreach($weeklyMenu['saturday-menu']['items'] as $item) --}}
-                {{--             <div class="list-item" style=""> --}}
-                {{--                 <div class="row"> --}}
-                {{--                     <div class="col-md-12"> --}}
-                {{--                         <h3 class="text-center">{{ $item }}</h3> --}}
-                {{--                     </div> --}}
-                {{--                 </div> --}}
-                {{--             </div> --}}
-                {{--         @endforeach --}}
-                {{--     </div> --}}
-                {{--     @if($weeklyMenu['saturday-menu'] != null)<h3 class="text-center">{{ $weeklyMenu['saturday-menu']['name'] }} <br><small>2300 Ft</small></h3>@endif --}}
-                {{-- </div> --}}
-            {{-- </div> --}}
-            {{-- <div class="col-md-6" style=""> --}}
-                {{-- <div class="text-left title-base"> --}}
-                {{--     <hr> --}}
-                {{--     <div class="list-items" style="margin-bottom: 25px;"> --}}
-                {{--         @foreach($weeklyMenu['sunday-menu']['items'] as $item) --}}
-                {{--             <div class="list-item" style=""> --}}
-                {{--                 <div class="row"> --}}
-                {{--                     <div class="col-md-12"> --}}
-                {{--                         <h3 class="text-center">{{ $item }}</h3> --}}
-                {{--                     </div> --}}
-                {{--                 </div> --}}
-                {{--             </div> --}}
-                {{--         @endforeach --}}
-                {{--     </div> --}}
-                {{--     @if($weeklyMenu['sunday-menu'] != null)<h3 class="text-center">{{ $weeklyMenu['sunday-menu']['name'] }} <br><small>2300 Ft</small></h3>@endif --}}
-                {{-- </div> --}}
-            {{-- </div> --}}
+             <div class="col-md-12" style="margin-bottom: 20px;"><hr></div> 
+             <div class="col-md-6" style=""> 
+                 <div class="text-left title-base"> 
+                     <hr> 
+                     <div class="list-items" style="margin-bottom: 25px;"> 
+                         @foreach($weeklyMenu['saturday-menu']['items'] as $item) 
+                             <div class="list-item" style=""> 
+                                 <div class="row"> 
+                                     <div class="col-md-12"> 
+                                         <h3 class="text-center">{{ $item }}</h3> 
+                                     </div> 
+                                 </div> 
+                             </div> 
+                         @endforeach 
+                     </div> 
+                     @if($weeklyMenu['saturday-menu'] != null)<h3 class="text-center">{{ $weeklyMenu['saturday-menu']['name'] }} <br><small>2300 Ft</small></h3>@endif 
+                 </div> 
+             </div> 
+             <div class="col-md-6" style=""> 
+                 <div class="text-left title-base"> 
+                     <hr> 
+                     <div class="list-items" style="margin-bottom: 25px;"> 
+                         @foreach($weeklyMenu['sunday-menu']['items'] as $item) 
+                             <div class="list-item" style=""> 
+                                 <div class="row"> 
+                                     <div class="col-md-12"> 
+                                         <h3 class="text-center">{{ $item }}</h3> 
+                                     </div> 
+                                 </div> 
+                             </div> 
+                         @endforeach 
+                     </div> 
+                     @if($weeklyMenu['sunday-menu'] != null)<h3 class="text-center">{{ $weeklyMenu['sunday-menu']['name'] }} <br><small>2300 Ft</small></h3>@endif 
+                </div> 
+            </div>
         </div>
         <div class="text-center">
             <a href="{{ route('pages.menu') }}" class="btn btn-sm">Étlap</a>

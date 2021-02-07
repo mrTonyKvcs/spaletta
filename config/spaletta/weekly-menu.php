a42fae0b2f89ae9f1bc4fad69b04723c6eb487a0
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-02-07 20:04:03 +0100

    new weekly menu 02.09-02.14

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,40 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2021.02.02 - 2021.02.07.',
+    'sub_title' => '2021.02.09 - 2021.02.14.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Grízgaluskaleves (L)',
-                'Bojtárleves (L)',
-                'Körtés zellerkrémleves (G)'
+                'Sárgaborsóleves , kolbászmorzsa (G,L)',
+                'Magyaros gombaleves, csibehússal (G,L)',
+                'Erőleves, tojáskocsonya (G,L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Sült bőrös malackaraj – barnamártás, krokett (L)',
-                'Burgonyafőzelék – zöldség fasírt (G, L)',
-                'Milánói sertésborda',
-                'Farsangi fánk – baracklekvár'
+                'Rizsfelfújt, meggyraguval',
+                'Sült pontypatkó , mexikói bulgur (L)',
+                'Chili con carne, friss házi lepény (L)',
+                'Serpenyős gyros (pulykából), hasábburgonya, saláta, tzatziki (G)'
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Ököruszály leves (L)',
-            'Brassói aprópecsenye – házi vegyes vágott (G, L)',
-            'Tiramisu'
+            'Póréhagymás burgonya- krémleves (G,L)',
+            'Filézett roston csirkecomb, baconos kukorica, karika burgonya (G,L)',
+            'Almás pite'
         ]
     ],
     'sunday-menu' => [
         'name'  => 'Vasárnapi menü',
         'items' => [
-            'Tárkonyos raguleves (G, L)',
-            'Chillis mézes oldalas – steak burgonya, saláta (G, L)',
-            'Kakaós palacsinta'
+            'Rozmaringos sertés raguleves (G)',
+            'Sertés Cordon Bleu , édesburgonya püré, párolt zöldségek',
+            'Valentin mákos álma'
         ]
     ]
 ];

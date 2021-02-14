8024464c6c0dde20a335f093f3f0fcea935898cf
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-02-14 23:52:33 +0100

    new weekly menu 02.16-02.21.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,40 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2021.02.09 - 2021.02.14.',
+    'sub_title' => '2021.02.16 - 2021.02.21.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Sárgaborsóleves , kolbászmorzsa (G,L)',
-                'Magyaros gombaleves, csibehússal (G,L)',
-                'Erőleves, tojáskocsonya (G,L)'
+                'Francia hagymakrémleves sajtos pirítós',
+                'Májgaluska leves (G, L)',
+                'Frankfurti leves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Rizsfelfújt, meggyraguval',
-                'Sült pontypatkó , mexikói bulgur (L)',
-                'Chili con carne, friss házi lepény (L)',
-                'Serpenyős gyros (pulykából), hasábburgonya, saláta, tzatziki (G)'
+                'Rablóhús – fóliás burgonya fokhagymás tejföl (G, L)',
+                'Csirkés ázsiai sülttészta (L)',
+                'Fahéjas szilvás gombóc',
+                'Csikóstokány – zsemlegombóc'
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Póréhagymás burgonya- krémleves (G,L)',
-            'Filézett roston csirkecomb, baconos kukorica, karika burgonya (G,L)',
-            'Almás pite'
+            'Lencsegulyás (G, L)',
+            'Kijevi jércemell – zöldfűszeres jázmin rizs',
+            'Francia csokisfánk'
         ]
     ],
     'sunday-menu' => [
         'name'  => 'Vasárnapi menü',
         'items' => [
-            'Rozmaringos sertés raguleves (G)',
-            'Sertés Cordon Bleu , édesburgonya püré, párolt zöldségek',
-            'Valentin mákos álma'
+            'Zöldségleves vajas galuskával (G)',
+            'Lecsós szelet - pirított burgonya (G, L)',
+            'Túrós kakaós boci szelet'
         ]
     ]
 ];

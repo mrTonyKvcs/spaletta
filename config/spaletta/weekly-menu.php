49b1cfa74dc2ea7cfc5afadbd2cfed1595b269d7
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-03-09 10:05:47 +0100

    new weekly menu 03.09-03.14.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,40 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2021.03.02 - 2021.03.07.',
+    'sub_title' => '2021.03.09 - 2021.03.14.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Erdélyi tárkonyos húsgombóc leves (L)',
-                'Rumos-csokoládés meggyleves (G)',
-                'Tyúkhúsleves csigatészta (L)'
+                'Jókai bableves (L)',
+                'Őszibarack krémleves (G,L)',
+                'PHO leves kacsahússal ( L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Tarjasteak zöldbors mártás – fűszeres cikkburgonya (G, L)',
-                'Rakott burgonya kolbásszal (G, L)',
-                'Zöldséges rizses sajtgolyó – tartár',
-                'Vaníliás kókuszos kelt csiga'
+                'Borjúpaprikás – orsótészta, kovászos uborka (L)',
+                'Csirkés tortilla – hasábburgonya (L)',
+                'Sajtos vagy mákos nudli',
+                'Zöldborsófőzelék – fóliába sült töltött fasírttekercs'
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Minestrone leves (G, L)',
-            'Töltött csirkecomb – petrezselymes burgonya (L)',
-            'Puncsszelet'
+            'Kelbimbó krémleves fásgaluska (L)',
+            'Chilis-mézes csirkeszárny, kevert saláta, jázminrizs (G, L)',
+            'Fanta szelet'
         ]
     ],
     'sunday-menu' => [
         'name'  => 'Vasárnapi menü',
         'items' => [
-            'Sütőtök krémleves bacon chips (G, L)',
-            'Sous - vide csirkemell – friss kevertsaláta (G, L)',
-            'Csokis narancsos palacsinta'
+            'Grízgaluskaleves (L)',
+            'Házi hurka-kolbász, tepsis burgonya, párolt káposzta(G, L)',
+            'Somlói galuska'
         ]
     ]
 ];

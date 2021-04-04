baedf00fb2566f63a8e2c120ff8161a64f7d15ae
KovacsJ92 <jozsef.kovacs92@gmail.com>
2021-04-04 16:07:16 +0200

    new weekly menu 04.06-04.11.

--- config/spaletta/weekly-menu.php
+++ config/spaletta/weekly-menu.php
@@ -2,40 +2,40 @@
 
 return [
     'title'     => 'heti menü',
-    'sub_title' => '2021.03.30 - 2021.04.04.',
+    'sub_title' => '2021.04.06 - 2021.04.11.',
     'menu' => [
         'soups' => [
             'name'  => 'Levesek',
             'items' => [
-                'Brokkoli krémleves (G, L)',
-                'Kínai csípős – savanyú leves (L, G)',
-                'Palócleves (L)'
+                'Medvehagymás burgonyakrémleves - bacon chips (G, L)',
+                'Legényfogó leves (G, L)',
+                'Rumos szilvaleves (G, L)'
             ]
         ],
         'main-course' => [
             'name'  => 'Főételek',
             'items' => [
-                'Pacalpörkölt – sós burgonya csemege uborka (G, L)',
-                'Aranygaluska vaníliasodó',
-                'Kelkáposzta főzelék – majoránnás fasírtlabda (L)',
-                'Pirosra sült csirkecomb – görögsaláta (G)'
+                'Tejszínes gombás pulykacomb – rizs (G, L)',
+                'Sertésborda Orly módra – pirított burgonya, lecsó (L)',
+                'Káposztás cvekedli (borsosan vagy cukrosan) (L)',
+                'Fogas „en papillote”- citrusos mártás (G)'
             ]
         ],
     ],
     'saturday-menu' => [
         'name'  => 'Szombati menü',
         'items' => [
-            'Tavaszi zöldségleves (L)',
-            'Csülök Pékné módra (G, L)',
-            'Tündérszelet'
+            'Csurgatott tojásleves (L)',
+            'Bardírozott csirke – répapüré, pároltzöldség (G)',
+            'Almás habos kocka'
         ]
     ],
     'sunday-menu' => [
         'name'  => 'Vasárnapi menü',
         'items' => [
-            'Húsvéti sonkaleves (G, L)',
-            'Habart tojással töltött sertésborda – rizibizi, gyümölcssaláta (G, L)',
-            'Tavaszi répatorta'
+            'Parmezános fokhagymakrémleves (G)',
+            'Holstein szelet – fűszeres steak burgonya, friss kevertsaláta (G, L)',
+            'Meggyes pite'
         ]
     ]
 ];
